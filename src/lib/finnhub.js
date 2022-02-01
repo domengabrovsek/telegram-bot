5bfa8658ac535970b21711ac338db6f4bbe3798d
Domen Gabrovsek <domen.gabrovsek@tretton37.com>
2022-02-02 00:47:10 +0100

    Added function to get symbol description and better format

--- src/lib/finnhub.js
+++ src/lib/finnhub.js
@@ -8,10 +8,35 @@ const options = {
   }
 };
 
+const getSymbolName = async (symbol) => {
+
+  const url = `${baseUrl}/search?q=${symbol}`;
+  const result = (await axios.get(url, options)).data;
+
+  const symbolObj = result.result.find(obj => obj.symbol === symbol);
+  const symbolName = symbolObj.description;
+
+  return symbolName;
+};
+
 const getSymbolInfo = async (symbol) => {
   const url = `${baseUrl}//quote?symbol=${symbol}`;
-  const result = await axios.get(url, options);
-  return result.data;
+  const result = (await axios.get(url, options)).data;
+
+  const symbolName = await getSymbolName(symbol);
+
+  const symbolInfo = {
+    name: symbolName,
+    current: result.c,
+    high: result.h,
+    low: result.l,
+    open: result.o,
+    change: result.d,
+    percentChange: result.dp,
+    previousClose: result.pc
+  };
+
+  return symbolInfo;
 };
 
 module.exports = {
