3c473df589be850a77ad8230aa719d17f32981c8
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2022-03-24 21:20:41 +0100

    Added serverless-webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: slsw.lib.entries,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.json'],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  externals: [
+    /aws-sdk/, // Available on AWS Lambda
+  ],
+};
