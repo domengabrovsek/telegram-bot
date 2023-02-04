d31c2be175d885d1f6ae2dd25b1bf52be06ec46f
Domen Gabrovšek <domen.gabrovsek@gmail.com>
2023-02-04 18:06:10 +0100

    remove all old files

--- src/lib/utils.js
+++
@@ -1,9 +0,0 @@
-const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-
-const queryStringify = (obj) => {
-  const key = Object.keys(obj)[0];
-  const value = obj[key];
-  return `${key}=${value}`;
-};
-
-module.exports = { getRandomNumber, queryStringify };
